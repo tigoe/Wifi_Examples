e9076aaf8d0e8fb267762b60cbdd6a1b46e41ee0
tigoe <tom.igoe@gmail.com>
2020-06-05 18:38:42 -0400

    Update UptimeServer.ino

--- UptimeServer/UptimeServer.ino
+++ UptimeServer/UptimeServer.ino
@@ -11,6 +11,7 @@
 */
 #include <SPI.h>
 #include <WiFi101.h>
+//#include <WiFiNINA.h>
 #include <WiFiUdp.h>
 #include <RTCZero.h>
 
@@ -24,11 +25,12 @@ char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as k
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
-const int GMT = -5; //change this to adapt to to your time zone
 unsigned long startTime;
-
 void setup() {
   Serial.begin(9600);
+  printWiFiStatus();
+
+  Serial.print("Attempting to connect to SSID: ");
 
   // attempt to connect to WiFi network:
   while ( status != WL_CONNECTED) {
@@ -47,28 +49,19 @@ void setup() {
   rtc.begin();
 
   unsigned long epoch;
-  int numberOfTries = 0, maxTries = 6;
   do {
-    Serial.print("Trying to get time, attempt number: ");
-    Serial.println(numberOfTries);
-    epoch = WiFi.getTime();
-    numberOfTries++;
-    delay(1000);
-  }
-  while ((epoch == 0) || (numberOfTries < maxTries));
+    Serial.println("Trying to get time");
 
-  if (numberOfTries > maxTries) {
-    Serial.print("NTP unreachable; couldn't get time");
-    // stop forever
-    while (1);
-  }
-  else {
-    Serial.print("Epoch received: ");
-    Serial.println(epoch);
-    rtc.setEpoch(epoch);
-    //set the epoch as the start time of the server:
-    startTime = epoch;
-  }
+    epoch = WiFi.getTime();
+    delay(1000);
+  } while (epoch == 0);
+
+
+  Serial.print("Epoch received: ");
+  Serial.println(epoch);
+  rtc.setEpoch(epoch);
+  //set the epoch as the start time of the server:
+  startTime = epoch;
 }
 
 void loop() {
@@ -92,16 +85,22 @@ void loop() {
           client.println("Connection: close");  // the connection will be closed after completion of the response
           client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
+          client.println(" <!DOCTYPE HTML > ");
+          client.println("<html>\n<head></head>\n<body>");
+          client.print("Firmware version: ");
+          client.println(WiFi.firmwareVersion());
+          client.println(" <br /> ");
+          client.print("RSSI: ");
+          client.println(WiFi.RSSI());
+          client.println(" <br /> ");
           client.print("current time: ");
           client.println(getNow());
-          client.println("<br />");
+          client.println(" <br /> ");
           client.print("uptime: ");
-          client.println(getUptime());
-          client.println("<br />");
-
-          client.println("</html>");
+          client.println(getUptime(1));
+          client.println(" <br /> ");
+     
+          client.println("</body></ html > ");
           break;
         }
         if (c == '\n') {
@@ -123,38 +122,64 @@ void loop() {
   }
 }
 
-String getUptime() {
+String getUptime(int format) {
   unsigned long upNow = rtc.getEpoch() - startTime;
   int upSecs = upNow % 60;
   int upMins = upNow % 3600L / 60;
   int upHours = upNow % 86400L / 3600;
-  int upDays = upNow % 2073600L / 86400L;
+  int upDays = upNow % 31556926L / 86400L;
   String uptime = format2Digits(upDays);
-  uptime += " days, ";
-  uptime += format2Digits(upHours);
-  uptime += ":";
-  uptime += format2Digits(upMins);
-  uptime += ":";
-  uptime += format2Digits(upSecs);
+  if (format == 0) { // short
+    uptime += ":";
+    uptime += format2Digits(upHours);
+    uptime += ":";
+    uptime += format2Digits(upMins);
+    uptime += ":";
+    uptime += format2Digits(upSecs);
+  } else { // long
+
+    uptime += " days, ";
+    uptime += format2Digits(upHours);
+    uptime += ": ";
+    uptime += format2Digits(upMins);
+    uptime += ": ";
+    uptime += format2Digits(upSecs);
+  }
   return uptime;
 }
+
 String getNow() {
   String now = format2Digits(rtc.getDay() );
-  now += "/";
+  now += " /";
   now += format2Digits(rtc.getMonth());
-  now += "/";
+  now += " /";
   now +=  format2Digits(rtc.getYear());
-  now += ",";
-  now += format2Digits(rtc.getHours() + GMT);
+  now += ", ";
+  now += format2Digits(rtc.getHours());
   now += ":";
   now += format2Digits(rtc.getMinutes());
   now += ":";
   now +=  format2Digits(rtc.getSeconds());
+  now += " UTC";
   return now;
 }
 
 
 void printWiFiStatus() {
+  // print firmware version:
+  Serial.print("WiFi firmware version: ");
+  Serial.println(WiFi.firmwareVersion());
+  // print MAC address:
+
+  Serial.print("MAC address: ");
+  byte mac[6];
+  WiFi.macAddress(mac);
+  for (int b = 5; b >= 0; b--) {
+    if (mac[b] < 15) Serial.print("0");
+    Serial.print(mac[b], HEX);
+    if (b > 0) Serial.print(": ");
+  }
+  Serial.println();
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -166,7 +191,7 @@ void printWiFiStatus() {
 
   // print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
+  Serial.print("signal strength (RSSI): ");
   Serial.print(rssi);
   Serial.println(" dBm");
 }
