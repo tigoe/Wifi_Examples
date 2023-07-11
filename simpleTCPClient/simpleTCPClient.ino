c5496a1ea241c7c29639080815ef4c141807312a
Tom Igoe <tom.igoe@gmail.com>
2023-07-11 12:36:29 +0200

    Update simpleTCPClient.ino

--- simpleTCPClient/simpleTCPClient.ino
+++ simpleTCPClient/simpleTCPClient.ino
@@ -1,30 +1,36 @@
 /*
-  A simple TCP Client
+  A simple TCP Client.
+  Opens a TCP socket and sends the time every ten seconds.
+
+  To test it, enter your laptop's address for the serverAddress variable,
+  then open netcat to listen on port 8080 like so:
+  $ nc -l 8080
 
   Uses the following libraries:
   http://librarymanager/All#WiFi101   // use this for MKR1000
   http://librarymanager/All#WiFiNINA    // use this for MKR1010 or Nano 33 IoT
+  http://librarymanager/All#WiFiS3    // use this for Uno R4 WiFi
 
   created 22 Oct 2018
-  updated 17 Jan 2021
+  updated 11 Jul 2023
   by Tom Igoe
 */
 #include <SPI.h>
-#include <WiFiNINA.h> // use this for MKR1010 board and Nano 33 IoT
-//#include <WiFi101.h>        // use this for the MKR1000 board
+// #include <WiFiNINA.h> // use this for MKR1010 board and Nano 33 IoT
+// #include <WiFi101.h>        // use this for the MKR1000 board
+#include <WiFiS3.h>        // use this for the Uno R4 WiFi board
 #include "arduino_secrets.h"
 
 // Initialize the Wifi client library
 WiFiClient client;
 // the address and port of the server
-const char serverAddress[] = "192.168.1.165";
+const char serverAddress[] = "10.130.22.70";
 int port = 8080;
 
 // request timestamp in ms:
 long lastRequest = 0;
 // interval between requests:
 int interval = 10000;
-
 
 void setup() {
   Serial.begin(9600);
@@ -45,6 +51,7 @@ void setup() {
 void loop() {
   // if you're not connected, try to connect:
   if (!client.connected()) {
+    Serial.println("connecting to server");
     client.connect(serverAddress, port);
   }
   // if you're connected, send every ten seconds:
