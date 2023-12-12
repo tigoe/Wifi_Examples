caaddec0eb71af8d067ea8badccd50c27c60d65e
Tom Igoe <tom.igoe@gmail.com>
2023-12-11 20:17:14 -0500

    Update simpleTCPServer.ino

--- simpleTCPServer/simpleTCPServer.ino
+++ simpleTCPServer/simpleTCPServer.ino
@@ -4,20 +4,24 @@
   http://librarymanager/All#WiFi101   // use this for MKR1000
   http://librarymanager/All#WiFiNINA    // use this for MKR1010 or Nano 33 IoT
 
+  For the Arduino Uno R4 WiFi, use
+  #include <WiFiS3.h>
+  For the Nano ESP32 use 
+  #include <WiFi.h>
+
   created 22 Oct 2018
-  updated 17 Jan 2021
+  updated 11 Dec 2023
   by Tom Igoe
 */
 #include <SPI.h>
-#include <WiFiNINA.h> // use this for MKR1010 board or Nano 33 IoT
-//#include <WiFi101.h>        // use this for the MKR1000 board
+#include <WiFiNINA.h>  // use this for MKR1010 board and Nano 33 IoT
+// #include <WiFi101.h>       // use this for the MKR1000 board
+// #include <WiFiS3.h>        // use this for the Uno R4 WiFi board
+// #include <WiFi.h>             // use this for the Nano ESP32 board
 #include "arduino_secrets.h"
 
-//// Initialize the Wifi client library
-//WiFiClient client;
-
-WiFiServer server(8889);
-boolean alreadyConnected = false; // whether or not the client was connected previously
+WiFiServer server(8080);
+boolean alreadyConnected = false;  // whether or not the client was connected previously
 
 void setup() {
   Serial.begin(9600);
@@ -25,8 +29,8 @@ void setup() {
   // while you're not connected to a WiFi AP,
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to Network named: ");
-    Serial.println(SECRET_SSID);          // print the network name (SSID)
-    WiFi.begin(SECRET_SSID, SECRET_PASS); // try to connect
+    Serial.println(SECRET_SSID);           // print the network name (SSID)
+    WiFi.begin(SECRET_SSID, SECRET_PASS);  // try to connect
     delay(2000);
   }
 
@@ -62,5 +66,4 @@ void loop() {
       Serial.write(thisChar);
     }
   }
-
 }
